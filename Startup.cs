645eae644a4b3e5077a7d901c30cdcd3cdea10c3
henriquedezani <dezani@gmail.com>
2019-03-29 07:44:19 -0300

    Liberado acesso público da API (CORS)

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace LeitorImagem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +41,12 @@ namespace LeitorImagem
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());   
 
             app.UseHttpsRedirection();
             app.UseMvc();
